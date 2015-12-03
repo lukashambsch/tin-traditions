219f8f3c94329c890ae8cb9d4f1543bcfbd96663
Lukas Hambsch <lukas.hambsch@gmail.com>
2015-12-03 13:49:38 -0700

    Added async validator for pin.

---
+++ client/js/item/pin-validator.directive.js
@@ -0,0 +1,21 @@
+(function () {
+  angular.module('app.item')
+    .directive('pinValidator', ['$http', '$q', pinValidator]);
+
+  function pinValidator ($http, $q) {
+    return {
+      require: 'ngModel',
+      link: function (scope, element, attrs, ngModel) {
+        ngModel.$asyncValidators.pin = function (modelValue, viewValue) {
+          return $http.post('api/giftEntries/check-pin', {giftEntry: {itemId: scope.entry.giftEntry.itemId, pin: viewValue}})
+            .then(function (response) {
+              if (!response.data.valid) {
+                return $q.reject(response.data.errorMessage);
+              }
+              return true;
+            });
+        }
+      }
+    }
+  }
+})();
--- common/models/gift-entry.js
+++ common/models/gift-entry.js
@@ -1,18 +1,40 @@
 var loopback = require('loopback');
 var Item = loopback.getModel('Item');
 
+var errorMessage = 'The PIN entered does not match the PIN for this item.';
+
 module.exports = function(GiftEntry) {
-  GiftEntry.validateAsync('pin', pinValidator, {message: 'The PIN entered does not match the PIN for this item.'});
+
+  GiftEntry.validateAsync('pin', pinValidator, {message: errorMessage});
+  GiftEntry.remoteMethod('checkPin', {
+    http: {path: '/check-pin'},
+    accepts: {arg: 'giftEntry', type: 'object'},
+    returns: [
+      {arg: 'valid', type: 'boolean'},
+      {arg: 'errorMessage', type: 'string'}
+    ]
+  });
 
   function pinValidator (error, done) {
-    console.log('validating');
     var base = this;
     entry = base.toJSON();
     dbItem = Item.findOne({id: entry.itemId}, function (err, item) {
+      if (err) throw err;
       if (base.pin != item.pin) error();
       base.pin = '';
       done();
     });
   }
+
+  GiftEntry.checkPin = function checkPin (giftEntry, cb) {
+    var valid = false;
+    Item.findById(giftEntry.itemId, function (err, item) {
+      if (item && item.pin == giftEntry.pin) {
+        valid = true;
+      }
+      var errMsg = valid ? '' : errorMessage;
+      cb(null, valid, errMsg);
+    });
+  };
 
 };
