3855372d03ae930c01c684f022244eaccc386aee
Lukas Hambsch <lukas.hambsch@gmail.com>
2016-01-27 09:12:28 -0800

    Removed (commented) old pin validators.

--- common/models/gift-entry.js
+++ common/models/gift-entry.js
@@ -4,38 +4,38 @@ var errorMessage = 'This user is not able to add entries to this item. Please li
 
 module.exports = function(GiftEntry) {
 
-  GiftEntry.validateAsync('pin', pinValidator, {message: errorMessage});
+  //GiftEntry.validateAsync('pin', pinValidator, {message: errorMessage});
   GiftEntry.validateAsync('item', userValidator, {message: errorMessage});
-  GiftEntry.remoteMethod('checkPin', {
-    http: {path: '/check-pin'},
-    accepts: {arg: 'giftEntry', type: 'object'},
-    returns: [
-      {arg: 'valid', type: 'boolean'},
-      {arg: 'errorMessage', type: 'string'}
-    ]
-  });
+  //GiftEntry.remoteMethod('checkPin', {
+    //http: {path: '/check-pin'},
+    //accepts: {arg: 'giftEntry', type: 'object'},
+    //returns: [
+      //{arg: 'valid', type: 'boolean'},
+      //{arg: 'errorMessage', type: 'string'}
+    //]
+  //});
 
-  function pinValidator (error, done) {
-    var base = this;
-    var entry = base.toJSON();
-    dbItem = Item.findOne({id: entry.itemId}, function (err, item) {
-      if (err) throw err;
-      if (base.pin != item.pin) error();
-      base.pin = '';
-      done();
-    });
-  }
+  //function pinValidator (error, done) {
+    //var base = this;
+    //var entry = base.toJSON();
+    //dbItem = Item.findOne({id: entry.itemId}, function (err, item) {
+      //if (err) throw err;
+      //if (base.pin != item.pin) error();
+      //base.pin = '';
+      //done();
+    //});
+  //}
 
-  GiftEntry.checkPin = function checkPin (giftEntry, cb) {
-    var valid = false;
-    Item.findById(giftEntry.itemId, function (err, item) {
-      if (item && item.pin == giftEntry.pin) {
-        valid = true;
-      }
-      var errMsg = valid ? '' : errorMessage;
-      cb(null, valid, errMsg);
-    });
-  };
+  //GiftEntry.checkPin = function checkPin (giftEntry, cb) {
+    //var valid = false;
+    //Item.findById(giftEntry.itemId, function (err, item) {
+      //if (item && item.pin == giftEntry.pin) {
+        //valid = true;
+      //}
+      //var errMsg = valid ? '' : errorMessage;
+      //cb(null, valid, errMsg);
+    //});
+  //};
 
   function userValidator (error, done) {
     var base = this;
