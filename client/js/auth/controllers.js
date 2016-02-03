797ce71e840cb52ffd556ee89925b568d9242902
Lukas Hambsch <lukas.hambsch@gmail.com>
2016-02-03 13:00:32 -0800

    Added user glyphicon if no image.

--- client/js/auth/controllers.js
+++ client/js/auth/controllers.js
@@ -5,6 +5,7 @@
   function UserController ($scope, $cookies, AuthFactory) {
     var vm = this;
     vm.currentUser = AuthFactory.currentUser;
+    vm.currentUserHasImage = currentUserHasImage;
 
     initialize();
 
@@ -23,6 +24,13 @@
 
     function getCurrentUser () {
       AuthFactory.ensureHasCurrentUser();
+    }
+
+    function currentUserHasImage () {
+      return (
+        vm.currentUser && vm.currentUser.profiles && vm.currentUser.profiles.length > 0 &&
+        vm.currentUser.profiles[0].profile.photos.length > 0 && vm.currentUser.profiles[0].profile.photos[0].value
+      );
     }
   }
 })();
